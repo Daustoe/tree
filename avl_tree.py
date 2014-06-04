7f4b1f3ea2d7112399f4f450d47b46fccf56b18e
Clayton Powell <daustoe@gmail.com>
2014-06-04 00:04:31 -0700

    Added some self balancing
    Not complete yet

--- avl_tree.py
+++ avl_tree.py
@@ -5,21 +5,26 @@ class Node(object):
     def __init__(self, data):
         self.left = None
         self.right = None
+        self.parent = None
         self.cargo = data
+        self.balance = 0
 
     def insert(self, data):
         if self.cargo > data:
+            self.balance += 1
             if self.left is not None:
                 self.left.insert(data)
             else:
                 self.left = Node(data)
-                return 1
+                self.left.parent = self
         else:
+            self.balance -= 1
             if self.right is not None:
                 self.right.insert(data)
             else:
                 self.right = Node(data)
-                return 1
+                self.right.parent = self
+        self.check_balance()
 
     def count(self):
         if self.left is None and self.right is None:
@@ -33,8 +38,56 @@ class Node(object):
                 right = self.right.count()
             return left + right + 1
 
+    def check_balance(self):
+        if self.balance == 2:  # left branch is deeper than right
+            if self.left is not None and self.left.balance == -1:  # Left child is deeper on right
+                self.left.rotate_left()
+            self.rotate_right()
+        elif self.balance == -2:  # right branch is deeper than left
+            if self.right is not None and self.right.balance == 1:  # right child is deeper on the left
+                self.right.rotate_right()
+            self.rotate_left()
 
-class Avl_Tree(object):
+    def point_to_me(self, node):
+        if node.cargo < self.cargo:
+            self.left = node
+        else:
+            self.right = node
+
+    def rotate_right(self):
+        # need to rebalance these rotated nodes
+        rotato = self.left
+        self.left = rotato.right
+        if self.left is not None:
+            self.left.parent = self
+        rotato.parent = self.parent
+        if type(self.parent) is not 'AvlTree':
+            self.parent.point_to_me(rotato)
+        self.parent = rotato
+        rotato.right = self
+        print rotato.balance
+        rotato.balance -= 1
+        self.balance = 0
+
+    def rotate_left(self):
+        print self.balance
+        rotato = self.right  # rotation potato # child node being rotated up # hot potato
+        self.right = rotato.left
+        rotato.parent = self.parent
+        self.parent = rotato
+        rotato.left = self
+
+    def traverse(self, attribute):
+        left = []
+        right = []
+        if self.left is not None:
+            left = self.left.traverse(attribute)
+        if self.right is not None:
+            right = self.right.traverse(attribute)
+        return left + [getattr(self, attribute)] + right
+
+
+class AvlTree(object):
     def __init__(self):
         self.root = None
 
@@ -44,8 +97,20 @@ class Avl_Tree(object):
         else:
             return self.root.count()
 
+    def __str__(self):
+        return 'AvlTree'
+
+    def point_to_me(self, node):
+        self.root = node
+
     def insert(self, data):
         if self.root is None:
             self.root = Node(data)
+            self.root.parent = self
         else:
             self.root.insert(data)
+
+    def traverse(self, attribute):
+        print hasattr(self.root, attribute)
+        if hasattr(self.root, attribute):
+            return self.root.traverse(attribute)
--- avl_tree_tests.py
+++ avl_tree_tests.py
@@ -1,32 +1,46 @@
 import unittest
 import avl_tree
 
+
 class NodeTests(unittest.TestCase):
     def setUp(self):
         self.node = avl_tree.Node(1)
 
-    def testCreation(self):
+    def test_creation(self):
         self.assertIsNotNone(self.node)
+
 
 class TreeTests(unittest.TestCase):
     def setUp(self):
-        self.tree = avl_tree.Avl_Tree()
+        self.tree = avl_tree.AvlTree()
 
-    def testCreation(self):
+    def test_creation(self):
         self.assertIsNotNone(self.tree)
 
-    def testInsert(self):
+    def test_insert(self):
         self.assertTrue(len(self.tree) == 0)
         self.tree.insert(1)
         self.assertTrue(len(self.tree) == 1)
         self.tree.insert(2)
         self.assertEqual(len(self.tree), 2)
 
-    def testBalancedInsert(self):
+    def test_balanced_insert(self):
         self.tree.insert(5)
         self.tree.insert(4)
+        self.assertEqual(self.tree.root.cargo, 5)
         self.tree.insert(3)
         self.assertEqual(self.tree.root.cargo, 4)
+        self.assertEqual(self.tree.root.left.cargo, 3)
+        self.assertEqual(self.tree.root.right.cargo, 5)
+        self.assertListEqual([3, 4, 5], self.tree.traverse('cargo'))
+        self.assertListEqual([0, 0, 0], self.tree.traverse('balance'))
+        self.tree.insert(1)
+        self.assertListEqual([1, 3, 4, 5], self.tree.traverse('cargo'))
+        self.assertListEqual([0, 1, 1, 0], self.tree.traverse('balance'))
+        self.tree.insert(0)
+
+        print self.tree.traverse('cargo')
+        self.assertEqual(self.tree.root.cargo, 3)
 
 if __name__ == '__main__':
     unittest.main()
